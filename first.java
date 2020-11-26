a4f9005858b64421fec563e192cdb425df59802a
Faiz Hameed <faizftb@gmail.com>
2020-11-26 14:53:45 +0530

    working setup java

--- first.java
+++ first.java
@@ -1,9 +1,9 @@
 
 public class first{
-  
-
-    public void shoudReturnHelloWorld(){
-        print("hello");
-    };
-
-};
+    public static void main(String[] args) {
+        int book;
+        book=3;
+        System.out.print(book);
+        System.out.print("Goodbye, World");
+    }
+};  
